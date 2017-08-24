38a81fee3823cbbfa556327fea5a7b0eef069709
Kristina <30863912+Kristina-D@users.noreply.github.com>
2017-08-24 20:31:54 +0200

    Add files via upload

--- Task4.rb
+++ Task4.rb
@@ -1,21 +1,25 @@
-#Lesson 2 - Alphabet
-characters = ["А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я"]
-alphabet ={}
-i =1
+#LTask 4 - Alphabet
+#via range:
+characters_range = ('А'..'Я')
+glasnie_array = ["А", "Е", "И", "О", "У", "Ы", "Э",	"Ю", "Я"]
 
-characters.each do |character|
-  alphabet[character] = i
-  i+=1
-end
-
-i = 1
 my_hash = {}
-alphabet.each do |letter, number|
-  if (i==1)||(i==6)||(i==7)||(i==10)||(i==16)||(i==21)||(i==29)||(i==31)||(i==32)|| (i==33)
-    my_hash[i] = letter
-  end
-	i += 1
+i = 0
+
+characters_range.each do |letter|
+	if letter = glasnie_array[i]
+		i += 1
+		my_hash[i] = letter
+	end
 end
 
 puts "Гласные  буквы:"
-my_hash.each {|key, value| puts value}
+my_hash.each {|key, value| puts "#{value} - #{key}"}
+
+#via each_with_index :
+puts "Гласные  буквы:"
+["А", "Е", "И", "О", "У", "Ы", "Э",	"Ю", "Я"].each_with_index do |value, index|
+  puts "#{value} - #{index}"
+end
+
+
