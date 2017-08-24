af29793b9323e89ccf816987f005f34b4e928548
Kristina <30863912+Kristina-D@users.noreply.github.com>
2017-08-24 11:38:45 +0200

    Add files via upload

---
+++ Task3.rb
@@ -0,0 +1,28 @@
+#Task 3 - Fibonacci number
+my_array = []
+i = 0
+n = 1
+sum = 0
+
+while sum + i < 100
+  sum = sum + i
+  i = n
+  n = [i, sum].max
+  my_array.push(n)
+end
+
+puts my_array
+
+#another way
+my_array2 = [1, 1]
+i = 0
+
+puts "Or in another way:"
+
+loop do
+  i = my_array2[-2] + my_array2[-1]
+  break if i > 100
+  my_array2<<i
+end
+
+puts my_array2
