4defb7fb8250273d7b9e7038641d36bb6dce7d89
Kristina <30863912+Kristina-D@users.noreply.github.com>
2017-08-24 20:35:26 +0200

    Add files via upload

--- Task4.rb
+++ Task4.rb
@@ -1,16 +1,16 @@
 #LTask 4 - Alphabet
 #via range:
 characters_range = ('А'..'Я')
-glasnie_array = ["А", "Е", "И", "О", "У", "Ы", "Э",	"Ю", "Я"]
+glasnie_array = ["А", "Е", "И", "О", "У", "Ы", "Э", "Ю", "Я"]
 
 my_hash = {}
 i = 0
 
 characters_range.each do |letter|
-	if letter = glasnie_array[i]
-		i += 1
-		my_hash[i] = letter
-	end
+  if letter = glasnie_array[i]
+    i += 1
+    my_hash[i] = letter
+  end
 end
 
 puts "Гласные  буквы:"
@@ -18,7 +18,7 @@ my_hash.each {|key, value| puts "#{value} - #{key}"}
 
 #via each_with_index :
 puts "Гласные  буквы:"
-["А", "Е", "И", "О", "У", "Ы", "Э",	"Ю", "Я"].each_with_index do |value, index|
+["А", "Е", "И", "О", "У", "Ы", "Э", "Ю", "Я"].each_with_index do |value, index|
   puts "#{value} - #{index}"
 end
 
